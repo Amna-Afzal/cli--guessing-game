de4f2b7aa9dd996fa4d5f39c23f2378f54f0bf58
AMNA <itsamna884@gmail.com>
2024-03-26 14:27:32 +0500

    CreatEd NumbeR GuessinG GaMe

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const  randomNumber = Math.floor (Math.random()*6+1);
+const answers = await inquirer.prompt([
+{
+    name:"userGuessesNumber",
+    type:"number",
+    message:"GUESS A NUMBER  BETWEEN 1-6 :",
+
+}
+]);
+if (answers.userGuessesNumber=== randomNumber){
+    console.log("CONGRATS! YOU GUESSED RIGHT" );
+}
+else{
+    console.log("YOU GUESSES WRONG TRY AGAIN");
+}
